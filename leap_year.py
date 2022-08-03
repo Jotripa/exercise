3a3db74d3da6150ea3167c6267c9af3677636ed2
Josef Triady <110474316+Jotripa@users.noreply.github.com>
2022-08-03 22:09:08 +0700

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,13 +1,13 @@
-#Input tahun
-year = int(input("Masukkan tahun: "))
-#Pengkondisian
+#Input year
+year = int(input("Input year: "))
+#Conditioning
 if year % 4 == 0:
     if year % 100 == 0:
         if year % 400 == 0:
-            print("Ini adalah tahun kabisat")
+            print("This is a Leap Year!")
         else:
-            print("Ini Bukanlah tahun kabisat")
+            print("This is not a Leap Year1")
     else:
-        print("Ini adalah tahun kabisat")    
+        print("This is a Leap Year!")    
 else:
-    print("Ini bukanlah tahun kabisat")    
+    print("This is not a Leap Year")    
